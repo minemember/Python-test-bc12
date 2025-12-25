d18fe0356ed60d28246bad7c8a65a7b245240adc
Chanpit Game <bloodhunter06@gmail.com>
2025-12-25 07:12:37 +0000

    Update!

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## this is my first script
 print("Hello World!")
 print("I am using  github with posit.could")
+
+def hello():
+  name = input("What is your name: ")
+  print(f"Hello {name}!")
+
+#test function
+hello()
